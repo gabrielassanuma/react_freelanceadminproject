b79210e70c46158ef9f39b06dcfc3935a21ec32e
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-03-20 09:42:28 -0300

    created routes for home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+    <Router>
+      <nav>
+        <ul>
+          <Link to='/'>Home</Link>
+          <Link to='/contact'>Contact</Link>
+          <Link to='/company'>Company</Link>
+          <Link to='/newproject'>New Project</Link>
+        </ul>
+      </nav>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/contact" element={<Contact/>}/>
+        <Route exact path="/company" element={<Company/>}/>
+        <Route exact path="/newproject" element={<NewProject/>}/>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
