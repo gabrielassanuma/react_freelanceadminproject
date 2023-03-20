2bd6b556b027c408cf25f5a98bfdafa79cd84320
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-03-20 10:11:46 -0300

    created component container

--- src/App.js
+++ src/App.js
@@ -4,25 +4,29 @@ import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
 
+import Container from './components/layout/Container'
+
 function App() {
   return (
     <Router>
       <nav>
         <ul>
-          <Link to='/'>Home</Link>
-          <Link to='/contact'>Contact</Link>
-          <Link to='/company'>Company</Link>
-          <Link to='/newproject'>New Project</Link>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/contact'>Contact</Link></li>
+          <li><Link to='/company'>Company</Link></li>
+          <li><Link to='/newproject'>New Project</Link></li>
         </ul>
       </nav>
-      <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/contact" element={<Contact/>}/>
-        <Route exact path="/company" element={<Company/>}/>
-        <Route exact path="/newproject" element={<NewProject/>}/>
-      </Routes>
+      <Container>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/company" element={<Company />} />
+          <Route exact path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
     </Router>
-  )
+  );
 }
 
 export default App;
