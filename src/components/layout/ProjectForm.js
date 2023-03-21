08418d90880d5c1bfb935a4dfbe3bcc82104e004
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-03-21 17:40:50 -0300

    setup project form and styles

--- src/components/layout/ProjectForm.js
+++ src/components/layout/ProjectForm.js
@@ -1,13 +1,15 @@
 import styles from './ProjectForm.module.css'
 
+import Input from '../form/Input'
+
 function ProjectForm() {
   return (
-    <form>
+    <form className={styles.containerProjectForm}>
       <div>
-        <input type="text" placeholder="Enter project name" />
+        <Input type="text" text="Project name:" name="name" placeholder="Insert the name of project" />
       </div>
       <div>
-        <input type="number" placeholder="Enter total value:" />
+        <Input type="number" text="Project budget:" name="budget" placeholder="Insert project budget" />
       </div>
       <div>
         <select name="category_id">
