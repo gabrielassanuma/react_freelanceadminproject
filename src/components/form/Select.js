e384a9de9314f30c1d00e3d369abb86bc4aeb38f
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-03-21 21:27:34 -0300

    changed backend port to 3004, set axios to fetch API and add projectTypes to select on form

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -1,12 +1,31 @@
 import styles from './Select.module.css'
 
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+
 function Select({text, name, option, handleOnChange, value}) {
+
+  const [projectTypes, setProjectTypes] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3004/projects_type')
+      .then(response => {
+        setProjectTypes(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className={styles.formControlSelect}>
       <label htmlFor={name}>{text}</label>
       <select name={name} id={name}>
-        <option>Select one option: </option>
-       </select>
+        <option>Select one option:</option>
+        {projectTypes.map(projectType => {
+          return <option key={projectType.id} value={projectType.id}>{projectType.type}</option>
+        })}
+      </select>
     </div>
   )
 }
