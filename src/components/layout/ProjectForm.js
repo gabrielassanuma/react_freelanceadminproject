6be40e5797814b843afb855ffd5f0a2ff57ed806
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-03-22 13:30:08 -0300

    setup form post

--- src/components/layout/ProjectForm.js
+++ src/components/layout/ProjectForm.js
@@ -19,16 +19,24 @@ function ProjectForm({ handleSubmit, btnText, projectData }) {
     setProject({ ...project, [e.target.name]: e.target.value })
   }
 
+  function handleCategory(e) {
+    setProject({ ...project, category: {
+      id: e.target.value,
+      name: e.target.options[e.targetselectedIndex].text,
+
+    } })
+  }
+
   return (
     <form onSubmit={submit} className={styles.containerProjectForm}>
       <div>
-        <Input type="text" text="Project name:" name="name" placeholder="Insert the name of project" />
+        <Input type="text" text="Project name:" name="name" placeholder="Insert the name of project" handleOnChange={handleChange} />
       </div>
       <div>
-        <Input type="number" text="Project budget:" name="budget" placeholder="Insert project budget" />
+        <Input type="number" text="Project budget:" name="budget" placeholder="Insert project budget" handleOnChange={handleChange}  />
       </div>
       <div>
-        <Select name="category_id" text="Select category :"  />
+        <Select name="category_id" text="Select category :"  handleOnChange={handleCategory}/>
       </div>
       <SubmitButton text={btnText}/>
     </form>
