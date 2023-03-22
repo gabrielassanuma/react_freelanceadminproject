9c860d29d27f32733b4f3cc8e71f7a97ef3c5257
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-03-22 12:21:15 -0300

    setup post form

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -2,7 +2,28 @@ import styles from './NewProject.module.css'
 
 import ProjectForm from '../layout/ProjectForm'
 
+import { useNavigate } from 'react-router-dom'
+
 function NewProject() {
+
+  const history = useNavigate()
+
+  function createPost(project) {
+    project.cost = 0
+    project.services = []
+
+    fetch("http://localhost:3004/projects", {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json'
+
+      },
+      body: JSON.stringify(project),  
+    }).then((resp => resp.json)
+    .then((data) => console.log(data))
+    ).catch(error => console.log(error))
+  }
+
   return (
     <div className={styles.containerNewProject}>
       <h1>Create a New Project</h1>
